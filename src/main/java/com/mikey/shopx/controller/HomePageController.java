719ead0cee774b2309b0292bf697cf3c3e6803f5
mikeliu <mikeliu2511@gmail.com>
2020-06-11 14:09:13 -0500

    create user-authorities table(many to many)

--- src/main/java/com/mikey/shopx/controller/HomePageController.java
+++ src/main/java/com/mikey/shopx/controller/HomePageController.java
@@ -1,8 +1,17 @@
 package com.mikey.shopx.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomePageController {
-
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String sayIndex() {
+        return "index";
+    }
+    @RequestMapping(value = "/aboutus", method = RequestMethod.GET)
+    public String sayAbout() {
+        return "about";
+    }
 }
--- src/main/java/com/mikey/shopx/model/User.java
+++ src/main/java/com/mikey/shopx/model/User.java
@@ -2,6 +2,8 @@ package com.mikey.shopx.model;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "user")
@@ -12,6 +14,47 @@ public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
+    private String emailId;
+    private String password;
+    private boolean enabled;
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_authorities",
+    joinColumns = @JoinColumn(name = "userId"),
+    inverseJoinColumns = @JoinColumn(name = "authorityId"))
+    private Set<Authorities> authorities = new HashSet<>();
+
+    public Set<Authorities> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<Authorities> authorities) {
+        this.authorities = authorities;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     @OneToOne(mappedBy = "user")
     Customer customer;
