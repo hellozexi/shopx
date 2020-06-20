79f42e0c68e8a826ecb0c55533dc85fa13e3a24c
mikeliu <liuzexi@wustl.edu>
2020-06-20 14:22:21 -0500

    addCustomer service

--- src/main/java/com/mikey/shopx/service/CustomerService.java
+++ src/main/java/com/mikey/shopx/service/CustomerService.java
@@ -4,7 +4,9 @@ import com.mikey.shopx.model.Cart;
 import com.mikey.shopx.model.Customer;
 import com.mikey.shopx.model.Role;
 import com.mikey.shopx.model.RoleName;
+import com.mikey.shopx.repository.CartRepo;
 import com.mikey.shopx.repository.CustomerRepo;
+import com.mikey.shopx.repository.RoleRepo;
 import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,13 +17,25 @@ public class CustomerService {
     @Autowired
     CustomerRepo customerRepo;
 
-//    public String addCustomer(Customer customer) {
-//        Role role = new Role();
-//        role.setName("ROLE_USER");
-//
-//        Cart cart = new Cart();
-//        cart.setCustomer(customer);
-//        customer.setCart(cart);
-//    }
+    @Autowired
+    CartRepo cartRepo;
+
+    @Autowired
+    RoleRepo roleRepo;
+
+    public String addCustomer(Customer customer) {
+        Role role = new Role();
+        role.setName("ROLE_USER");
+        role.setEmail(customer.getUser().getEmail());
+
+        Cart cart = new Cart();
+        cart.setCustomer(customer);
+        customer.setCart(cart);
+
+        customerRepo.save(customer);
+        cartRepo.save(cart);
+        roleRepo.save(role);
+        return "Customer successfully added";
+    }
 
 }
